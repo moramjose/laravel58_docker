5dbf98a20e2554fed039498e7f5b625cd541e138
Jose Mora <jmoraw7@gmail.com>
2025-11-26 17:41:01 +0000

    guest guard (bloquear url de login y registro)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,19 +5,31 @@ import { RegisterComponent } from './components/register/register.component';
 import { AuthorsComponent } from './components/authors/authors.component';
 import { BooksComponent } from './components/books/books.component';
 import { authGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/guest.guard'; // <--- 1. IMPORTAR
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  // rutas publicas
+  { 
+    path: 'login', 
+    component: LoginComponent, 
+    canActivate: [guestGuard] // <--- 2. AGREGAR AQUÍ
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent, 
+    canActivate: [guestGuard] // <--- 3. AGREGAR AQUÍ
+  },
+  
+  // rutas privadas
   { 
     path: 'authors', 
     component: AuthorsComponent, 
-    canActivate: [authGuard]
+    canActivate: [authGuard] 
   },
   { 
     path: 'books', 
     component: BooksComponent, 
-    canActivate: [authGuard]
+    canActivate: [authGuard] 
   },
   
   { path: '**', redirectTo: 'login' }
